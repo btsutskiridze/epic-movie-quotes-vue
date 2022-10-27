6f027db6e85775718adf4936bd953dcaa094ca30
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-28 01:45:00 +0400

    feat: message * base bg image component *created  responsive landing page

---
+++ src/components/UI/landing-page/BaseBackgroundImg.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  fixed: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+  first: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="bg-no-repeat h-96 bg-cover md:h-screen"
+    :class="[fixed ? 'md:bg-fixed' : '', url, `bg-center md:bg-${position}`]"
+  >
+    <div
+      class="h-full flex justify-center flex-row items-center text-white font-montserrat font-bold"
+      :class="[first ? 'bg-linear-top-left' : ' media-bg-linear-top-left']"
+    >
+      <div class="w-[72%] md:w-[70%] -top-5 md:-top-20 relative mx-auto">
+        <div>
+          <article class="text-xl md:text-[3.1rem] relative leading-normal">
+            <slot name="quote"></slot>
+            <div
+              class="h-[2px] bg-white w-4 md:w-14 absolute top-4 md:top-10 -left-6 md:left-[-4.2rem]"
+            ></div>
+          </article>
+
+          <p class="mt-4 text-base md:text-3xl">
+            <slot name="movieName"></slot>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,60 @@
-<script setup></script>
+<script setup>
+import TheHeader from "@/components/layout/TheHeader.vue";
+import TheFooter from "@/components/layout/TheFooter.vue";
+import BaseButton from "@/components/UI/BaseButton.vue";
+import BaseBackgroundImg from "@/components/UI/landing-page/BaseBackgroundImg.vue";
+</script>
 
 <template>
-  <div class="flex justify-center">movie quotes</div>
+  <div class="bg-[#11101A] text-[#DDCCAA] font-helvetica">
+    <div class="relative z-40">
+      <the-header />
+      <div class="flex justify-center items-center gap-6 flex-col h-[60vh]">
+        <h1
+          class="text-2xl md:text-[4.3rem] text-center font-bold leading-normal"
+        >
+          Find any quote in <br />millions of movie lines
+        </h1>
+        <base-button :orange="true" class="md:text-xl">
+          Get started
+        </base-button>
+      </div>
+    </div>
+    <base-background-img
+      url="bg-[url('@/assets/images/landing-page/interstellar.png')]"
+      position="bottom"
+      :first="true"
+      :fixed="false"
+    >
+      <template #quote>
+        “You have to leave somethig <br />
+        behind to go forward”
+      </template>
+      <template #movieName>Interstellar, 2014</template>
+    </base-background-img>
+
+    <base-background-img
+      url="bg-[url('@/assets/images/landing-page/the-royal-tenenbaums.png')]"
+      position="center"
+    >
+      <template #quote>
+        I think we're just gonna have <br />
+        to be secretly in love with earch other <br />
+        and leave it that
+      </template>
+      <template #movieName> The Royal Tenenbaums,2001 </template>
+    </base-background-img>
+
+    <base-background-img
+      url="bg-[url('@/assets/images/landing-page/lord-of-rings.jpg')]"
+      position="top"
+    >
+      <template #quote>
+        All we have to decide is what to do <br />
+        with the time that is given to us.
+      </template>
+      <template #movieName> The Lord of the Rings, 2001 </template>
+    </base-background-img>
+    <the-footer />
+  </div>
 </template>
