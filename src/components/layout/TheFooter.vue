f7bc1a2c0c00f223fe3a59ccb15fd6abf8d125b6
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-28 01:42:35 +0400

    feat: landing header and footer

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="w-[90%] m-auto">
+    <h1 class="py-4">© 2022 movie quotes. All rights reserved.</h1>
+  </footer>
+</template>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,24 @@
+<script setup>
+import DropdownArrowIcon from "@/components/icons/DropdownArrowIcon.vue";
+import BaseButton from "@/components/UI/BaseButton.vue";
+</script>
+
+<template>
+  <header class="w-4/5 m-auto">
+    <div class="flex justify-between items-center py-6 text-base">
+      <h2 class="uppercase">movie quotes</h2>
+      <div class="flex justify-center items-center gap-4">
+        <ul class="w-24 text-center hidden md:block">
+          <li class="gap-3 flex flex-row justify-center items-center">
+            Eng <DropdownArrowIcon />
+          </li>
+          <li v-if="false">ka</li>
+        </ul>
+        <base-button :orange="true" class="hidden md:block">
+          Sign Up
+        </base-button>
+        <base-button :outline="true"> Log In </base-button>
+      </div>
+    </div>
+  </header>
+</template>
