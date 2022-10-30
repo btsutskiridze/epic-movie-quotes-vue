dfae6ef198cffef0d03ab439634ace4fa9c7f850
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-30 20:17:59 +0400

    feat: message *container of regisration form *hiding and displaying container using provide/inject

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,7 +1,14 @@
 <script setup>
 import LanguageDropdown from "@/components/layout/LanguageDropdown.vue";
-
+import RegistrationForm from "@/components/layout/form/RegistrationForm.vue";
 import BaseButton from "@/components/UI/BaseButton.vue";
+import { provide, ref } from "vue";
+
+const show = ref(false);
+const openRegistration = () => {
+  show.value = true;
+};
+provide("show", show);
 </script>
 
 <template>
@@ -10,7 +17,11 @@ import BaseButton from "@/components/UI/BaseButton.vue";
       <h2 class="font-bold uppercase">{{ $t("landingView.movie_quotes") }}</h2>
       <div class="flex justify-center items-center gap-4">
         <language-dropdown />
-        <base-button :orange="true" class="hidden md:block">
+        <base-button
+          :orange="true"
+          class="hidden md:block"
+          @click="openRegistration"
+        >
           {{ $t("landingView.sign_up") }}
         </base-button>
         <base-button :outline="true"
@@ -19,4 +30,5 @@ import BaseButton from "@/components/UI/BaseButton.vue";
       </div>
     </div>
   </header>
+  <RegistrationForm />
 </template>
