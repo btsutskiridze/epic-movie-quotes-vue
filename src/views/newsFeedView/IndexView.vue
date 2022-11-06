6a18bf78eb167f5af6e7d253a2a634f716011941
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-06 18:27:17 +0400

    feat: functional to logout

--- src/views/newsFeedView/IndexView.vue
+++ src/views/newsFeedView/IndexView.vue
@@ -2,15 +2,14 @@
 import router from "@/router/index.js";
 import { deleteJwtToken } from "@/helpers/jwt";
 const handleLogout = () => {
-  deleteJwtToken("jwt_token", "/", import.meta.DOMAIN);
+  deleteJwtToken("jwt_token", "/", import.meta.VITE_DOMAIN);
   router.push("/");
 };
 </script>
 
 <template>
   <div>
-    <h1>this is newsFeed nigga</h1>
-    <h2>shut up nigga</h2>
-    <h1 class="text-2xl cursor-pointer" @click="handleLogout">Log out</h1>
+    <h1>news feed</h1>
+    <h1 @click="handleLogout">LOG OUT</h1>
   </div>
 </template>
