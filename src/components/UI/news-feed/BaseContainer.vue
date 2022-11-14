e9b3135360bddd6f250895b1bdc5116814d491dd
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-14 23:38:02 +0400

    feat: message *base container for news feed *changing button color according to route

---
+++ src/components/UI/news-feed/BaseContainer.vue
@@ -0,0 +1,14 @@
+<script setup>
+import PcNavbar from "@/components/layout/news-feed/navbar/PcNavbar.vue";
+</script>
+
+<template>
+  <div class="w-full flex justify-start items-start text-white md:py-8">
+    <div class="w-[30%] hidden md:block sticky top-10">
+      <pc-navbar />
+    </div>
+    <div class="w-full md:w-[64%] lg:w-[50%] flex flex-col md:gap-4 pb-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
--- src/views/newsFeedView/NewsFeedBody.vue
+++ src/views/newsFeedView/NewsFeedBody.vue
@@ -1,7 +1,7 @@
 <script setup>
 import BasePost from "@/components/UI/news-feed/post/BasePost.vue";
+import BaseContainer from "@/components/UI/news-feed/BaseContainer.vue";
 
-import PcNavbar from "@/components/layout/news-feed/navbar/PcNavbar.vue";
 import PcQuoteButton from "@/components/layout/news-feed/new-quote-button/PcQuoteButton.vue";
 import MobileQuoteButton from "@/components/layout/news-feed/new-quote-button/MobileQuoteButton.vue";
 import PcSearch from "@/components/layout/news-feed/search-bar/PcSearch.vue";
@@ -47,26 +47,21 @@ const quotes = [
 </script>
 
 <template>
-  <div class="w-full flex justify-start items-start text-white md:py-8">
-    <div class="w-[30%] hidden md:block sticky top-10">
-      <pc-navbar />
+  <base-container>
+    <div class="flex-row items-center hidden md:flex gap-4">
+      <pc-quote-button />
+      <pc-search />
     </div>
-    <div class="w-full md:w-[64%] lg:w-[50%] flex flex-col md:gap-4 pb-4">
-      <div class="flex-row items-center hidden md:flex gap-4">
-        <pc-quote-button />
-        <pc-search />
-      </div>
-      <div class="block md:hidden">
-        <mobile-quote-button />
-      </div>
+    <div class="block md:hidden">
+      <mobile-quote-button />
+    </div>
 
-      <div id="posts" class="flex flex-col gap-8">
-        <base-post
-          v-for="quote in quotes"
-          :key="quote.id"
-          :quote="quote"
-        ></base-post>
-      </div>
+    <div id="posts" class="flex flex-col gap-8">
+      <base-post
+        v-for="quote in quotes"
+        :key="quote.id"
+        :quote="quote"
+      ></base-post>
     </div>
-  </div>
+  </base-container>
 </template>
