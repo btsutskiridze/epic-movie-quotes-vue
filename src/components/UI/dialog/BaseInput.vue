9663dd3a27b583d43fedad058acddf402e136a6b
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-17 19:40:16 +0400

    feat: message *create base input for dialogs *add rules props to textarea

--- src/components/UI/dialog/BaseInput.vue
+++ src/components/UI/dialog/BaseInput.vue
@@ -1,10 +1,20 @@
 <script setup>
 import { Field } from "vee-validate";
 
+// import { ref } from "vue";
 defineProps({
   name: {
     type: String,
     required: true,
+  },
+  type: {
+    type: String,
+    reuqired: false,
+    default: "text",
+  },
+  rules: {
+    type: String,
+    required: false,
   },
   placeholder: {
     type: String,
@@ -14,37 +24,27 @@ defineProps({
     type: String,
     required: false,
   },
-  rules: {
-    type: String,
-    required: false,
-  },
 });
-
-const autoResize = (e) => {
-  e.target.style.height = "auto";
-  e.target.style.height = e.target.scrollHeight + "px";
-};
 </script>
 
 <template>
   <div class="relative">
     <label :for="name" class="absolute right-4 top-2">{{ lang }}</label>
-    <Field v-slot="{ field, meta }" :name="name" :rules="rules">
-      <textarea
+    <Field :name="name" v-slot="{ field, meta }" :rules="rules">
+      <input
         v-bind="field"
-        rows="1"
-        cols="50"
-        :name="name"
         :id="name"
-        :placeholder="placeholder"
-        autocomplete="off"
-        @input="autoResize"
+        :type="type"
+        :placeholder="!lang ? $t(placeholder) : placeholder"
         class="relative bg-transparent resize-none overflow-y-hidden border rounded-[0.25rem] border-[#6C757D] pl-2 pr-12 py-2 text-base focus:outline-none hover:outline-none placeholder-[#6C757D] w-full"
         :class="[
           !meta.valid && meta.touched ? 'border-[#DC3545]' : '',
           meta.valid && meta.touched ? 'border-[#198754]' : '',
         ]"
+        autocomplete="off"
       />
     </Field>
   </div>
 </template>
+
+<style></style>
