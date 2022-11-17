f21ecc7c86a2e1af44113b575ab6ee3fa8caeaca
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-17 21:33:12 +0400

    feat: loading movie genres

--- src/views/moviesView/movieView/IndexView.vue
+++ src/views/moviesView/movieView/IndexView.vue
@@ -1,20 +1,34 @@
 <script setup>
 import { useRoute } from "vue-router";
 import axios from "@/config/axios/index.js";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import { useMoviesStore } from "@/stores/useMoviesStore";
 
+const store = useMoviesStore();
+
+const url = store.url;
 const movie = ref({});
+const genres = ref([]);
 
-axios.get("movies/" + useRoute().params.id).then((response) => {
-  movie.value = response.data;
+onMounted(() => {
+  axios.get("movies/" + useRoute().params.id).then((response) => {
+    movie.value = response.data;
+    genres.value = JSON.parse(movie.value.genre);
+  });
 });
 </script>
 
 <template>
   <div>
+    <router-link :to="{ name: 'add-quote' }">add quote</router-link>
     <h1>{{ movie.title?.en }}</h1>
+    <img :src="url + movie?.thumbnail" :alt="movie.name" />
     <h1>{{ movie.description?.en }}</h1>
     <h1>{{ movie.director?.en }}</h1>
     <h1>{{ movie.year }}</h1>
+    <h1 class="text-2xl">movie genre:</h1>
+    <h1 v-for="genre in genres" :key="genre.label">
+      {{ genre }}
+    </h1>
   </div>
 </template>
