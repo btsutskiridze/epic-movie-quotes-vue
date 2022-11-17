7373a241323c639b8f3ee7985d418a87e36dce8e
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-17 21:01:06 +0400

    feat: fetching single movie item

---
+++ src/views/moviesView/movieView/IndexView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from "vue-router";
+import axios from "@/config/axios/index.js";
+import { ref } from "vue";
+
+const movie = ref({});
+
+axios.get("movies/" + useRoute().params.id).then((response) => {
+  movie.value = response.data;
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{ movie.title?.en }}</h1>
+    <h1>{{ movie.description?.en }}</h1>
+    <h1>{{ movie.director?.en }}</h1>
+    <h1>{{ movie.year }}</h1>
+  </div>
+</template>
