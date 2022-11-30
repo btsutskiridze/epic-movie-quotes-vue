6562183d911fb4ede94a55fb3bc56f35ac89fb8b
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-30 20:29:55 +0400

    style: notification menu

---
+++ src/components/layout/notification/NotificationItem.vue
@@ -0,0 +1,32 @@
+<script setup>
+import QuoteIcon from "@/components/icons/movies/QuoteIcon.vue";
+import likedIcon from "@/components/icons/quotes/LikedIcon.vue";
+</script>
+
+<template>
+  <div
+    class="text-lg p-3 mb-4 flex flex-row gap-4 items-center justify-between w-full h-[5.2rem] rounded-[0.25rem] border border-[#363A3E]"
+  >
+    <div class="flex flex-row gap-4 items-center">
+      <img
+        src="@/assets/images/news-feed/avatar.png"
+        alt="avatar"
+        width="68"
+        height="68"
+        class="w-[60px] h-[60px]"
+      />
+      <div class="flex flex-col gap-1">
+        <h1>Brad Piti</h1>
+
+        <p class="flex flex-row items-center gap-2 text-gray-400">
+          <liked-icon class="w-[1.2rem] h-[1.2rem]" />
+          <span class="leading-none"> Reacted to your quote</span>
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-col items-end">
+      <h1>5 min ago</h1>
+      <h1 class="text-green-600">New</h1>
+    </div>
+  </div>
+</template>
--- src/components/layout/notification/NotificationsContainer.vue
+++ src/components/layout/notification/NotificationsContainer.vue
@@ -1,7 +1,44 @@
 <script setup>
 import NotificationIcon from "@/components/icons/news-feed/NotificationIcon.vue";
+import NotificationItem from "@/components/layout/notification/NotificationItem.vue";
+import { ref } from "vue";
+const show = ref(false);
 </script>
 
 <template>
-  <notification-icon />
+  <div
+    class="fixed w-screen h-screen top-0 left-0 z-40"
+    v-if="show"
+    @click="show = false"
+  ></div>
+  <div class="relative">
+    <notification-icon class="relative cursor-pointer" @click="show = !show" />
+    <span
+      @click="show = !show"
+      class="cursor-pointer absolute flex items-center justify-center text-xs top-[-0.25rem] right-0 bg-red-500 w-4 h-4 rounded-full"
+      >2</span
+    >
+    <div
+      v-if="show"
+      class="top-[2.3rem] absolute w-0 h-0 border-l-[10px] border-l-transparent border-b-[25px] border-b-black border-r-[10px] border-r-transparent"
+    ></div>
+  </div>
+  <div
+    v-if="show"
+    class="cursor-default flex flex-col gap-6 px-6 py-10 rounded-[0.25rem] z-[50] absolute top-[5rem] md:top-[5.6rem] bg-[#000] right-[7%] min-w-[44%]"
+  >
+    <section class="text-white flex flex-row justify-between">
+      <h1 class="text-[1.8rem]">Notifications</h1>
+      <h2 class="text-lg underline text-white cursor-pointer">
+        Mark as all read
+      </h2>
+    </section>
+    <section class="h-[20rem] overflow-y-scroll">
+      <notification-item />
+      <notification-item />
+      <notification-item />
+      <notification-item />
+      <notification-item />
+    </section>
+  </div>
 </template>
