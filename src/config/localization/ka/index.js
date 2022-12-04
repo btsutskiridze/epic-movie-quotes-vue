09ca38c7ba114f6476e74eb2d59fe1b510201151
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-12-04 20:26:15 +0400

    fix: seperated translations into json files

---
+++ src/config/localization/ka/index.js
@@ -0,0 +1,22 @@
+import landingView from "./landing.json";
+import quotes from "./quotes.json";
+import form from "./form.json";
+import verification from "./verification.json";
+import passwordReset from "./password-reset.json";
+import newsFeed from "./news-feed.json";
+import search from "./search.json";
+import fileInput from "./file-input.json";
+import movies from "./movies.json";
+
+const translations = {
+  landingView,
+  quotes,
+  form,
+  verification,
+  passwordReset,
+  newsFeed,
+  search,
+  fileInput,
+  movies,
+};
+export default translations;
--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,13 @@
-import ka from "@/config/localization/ka.js";
-import en from "@/config/localization/en.js";
+import ka from "@/config/localization/ka/index.js";
+import en from "@/config/localization/en/index.js";
 
 import { createI18n } from "vue-i18n";
 const i18n = createI18n({
   locale: "en", // set locale
   fallbackLocale: "en", // set fallback locale
   messages: {
-    ...en,
-    ...ka,
+    en,
+    ka,
   },
 });
 
