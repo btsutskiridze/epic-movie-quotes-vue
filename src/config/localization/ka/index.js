62a7a41652842f6be0f14387badfd15505ca2d58
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-12-05 14:34:58 +0400

    fix: fullpath

--- src/config/localization/ka/index.js
+++ src/config/localization/ka/index.js
@@ -4,13 +4,13 @@ import quote from "./quote.json";
 import form from "./form.json";
 import verification from "./verification.json";
 import passwordReset from "./password-reset.json";
-import newsFeed from "./news-feed.json";
-import search from "./search.json";
-import fileInput from "./file-input.json";
-import movies from "./movies.json";
-import genres from "./genres.json";
-import profile from "./profile.json";
-import notif from "./notifications.json";
+import newsFeed from "@/config/localization/ka/news-feed.json";
+import search from "@/config/localization/ka/search.json";
+import fileInput from "@/config/localization/ka/file-input.json";
+import movies from "@/config/localization/ka/movies.json";
+import genres from "@/config/localization/ka/genres.json";
+import profile from "@/config/localization/ka/profile.json";
+import notif from "@/config/localization/ka/notifications.json";
 
 const translations = {
   landingView,
