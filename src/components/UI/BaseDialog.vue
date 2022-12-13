cf8dfe0dd0bf40ee774127d4786acf3cf9dd1014
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-12-13 04:11:48 +0400

    style: dialogs

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,5 +1,20 @@
 <script setup>
+import { onMounted, onUnmounted, computed, ref } from "vue";
+
 const emit = defineEmits(["close"]);
+const clientHeight = computed(() => document.documentElement.clientHeight);
+const dialogHeight = ref(undefined);
+
+onMounted(() => {
+  dialogHeight.value = document.getElementById("dialog").offsetHeight;
+  document.body.style.position = "fixed";
+  document.body.style.width = "100vw";
+});
+
+onUnmounted(() => {
+  document.body.style.position = "unset";
+  document.body.style.width = "auto";
+});
 
 const closeDialog = (e) => {
   if (document.getElementById("container") === e.target) {
@@ -14,11 +29,13 @@ const closeDialog = (e) => {
       <div
         @click="closeDialog"
         id="container"
-        class="h-screen w-screen fixed top-0 left-0 z-30 backdrop-blur-sm bg-[#00000089] flex justify-center items-center"
+        class="fixed top-0 left-0 z-30 flex h-screen w-screen items-center justify-center overflow-y-scroll bg-[#00000089] backdrop-blur-sm"
       >
         <dialog
           open
-          class="bg-[#222030] w-full h-screen sm:min-w-[37.5rem] overflow-x-hidden sm:max-w-[32%] sm:h-min sm:rounded-xl z-40 px-9 pt-10 md:pt-2 sm:px-[7rem]"
+          id="dialog"
+          :class="dialogHeight > clientHeight ? 'top-0' : ''"
+          class="z-40 h-screen w-full overflow-x-hidden bg-[#222030] px-9 pt-10 sm:h-min sm:min-w-[37.5rem] sm:max-w-[32%] sm:rounded-xl sm:px-[7rem] md:pt-2"
         >
           <slot></slot>
         </dialog>
